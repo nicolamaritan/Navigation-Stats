11526a232eb0bb46fde78078537c957900b39755
Nicola Maritan <nicolamaritan01@gmail.com>
2023-02-12 15:20:44 +0100

    Solved popup issue

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,24 +1,22 @@
 function displaySessionData() {
   chrome.storage.session.get(null, function(data) {
-    console.log(data);
+    displayTable(data);
   });
-  displayTable(data);
 }
 
 function displayTable(dict) {
-  var keys = Object.keys(dict);
-  var values = Object.values(dict);
-
-  var table = "<table><tr><th>Key</th><th>Value</th></tr>";
-
-  for (var i = 0; i < keys.length; i++)
-  {
-    table += "<tr><td>" + keys[i] + "</td><td>" + values[i] + "</td></tr>";
+  let table = document.createElement('table');
+  for (let key in dict) {
+    let row = document.createElement('tr');
+    let cell1 = document.createElement('td');
+    cell1.innerHTML = key;
+    let cell2 = document.createElement('td');
+    cell2.innerHTML = dict[key];
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    table.appendChild(row);
   }
-
-  table += "</table>";
-
-  document.write(table);
+  document.body.appendChild(table);
 }
   
 displaySessionData();
