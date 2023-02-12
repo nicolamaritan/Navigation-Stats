cd36edba9a732f6364bcdacde03f8154e80c60f3
Nicola Maritan <nicolamaritan01@gmail.com>
2023-02-12 19:09:50 +0100

    Updated CSS

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,9 +5,10 @@ function displaySessionData() {
 }
 
 
-function displayTable(dict, rows) {
+function displayTable2(dict, rows) {
   let table = document.createElement('table');
-  
+  table.classList.add('main-table');
+    
   // Sort by values (visits) in descending order
   let entries = Object.entries(dict);
   entries.sort(function(a, b) {return b[1] - a[1];});
@@ -17,6 +18,7 @@ function displayTable(dict, rows) {
   {
     let [key, value] = entries[i];
     let row = document.createElement('tr');
+    row.classList.add('main-row');
     let cell1 = document.createElement('td');
     cell1.innerHTML = key;
     let cell2 = document.createElement('td');
@@ -26,6 +28,44 @@ function displayTable(dict, rows) {
     table.appendChild(row);
   }
   document.body.appendChild(table);
+}
+
+function displayTable(dictionary, rows) {
+  // Create a styles element and add it to the head of the document
+  const styles = document.createElement("style");
+  document.head.appendChild(styles);
+
+  // Sort the entries in descending order of their integer value
+  const entries = Object.entries(dictionary);
+  entries.sort((a, b) => parseInt(b[1]) - parseInt(a[1]));
+
+  let count = 0;
+  for (const entry of entries) {
+    if (count >= rows) break;
+
+    // Create a div to hold the key-value pair
+    const entryWrapper = document.createElement("div");
+    entryWrapper.classList.add("entry-wrapper");
+
+    // Create the key element
+    const keyElement = document.createElement("div");
+    keyElement.innerHTML = entry[0];
+    keyElement.classList.add("key");
+
+    // Create the value element
+    const valueElement = document.createElement("div");
+    valueElement.innerHTML = entry[1];
+    valueElement.classList.add("value");
+
+    // Append the key and value elements to the entry wrapper
+    entryWrapper.appendChild(keyElement);
+    entryWrapper.appendChild(valueElement);
+
+    // Add the entry wrapper to the page
+    document.body.appendChild(entryWrapper);
+
+    count++;
+  }
 }
 
 
